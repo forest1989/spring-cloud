1335a269c43e6e6cb6492a33f1b56aaaea5f7e86
forest <forest@192.168.226.1>
2018-12-03 17:58:00 +0800

    1

---
+++ forest-api/src/main/java/com/forest/api/common/druid/DruidConfiguration.java
@@ -0,0 +1,43 @@
+package com.forest.api.common.druid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+
+@Configuration
+public class DruidConfiguration {
+	private static final Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
+	 
+    // private static final String DB_PREFIX = "spring.datasource";
+ 
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidServlet() {
+        logger.info("init Druid Servlet Configuration ");
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<StatViewServlet>(new StatViewServlet(), "/druid/*");
+        // IP白名单
+        // servletRegistrationBean.addInitParameter("allow", "*");
+        // IP黑名单(共同存在时，deny优先于allow)
+        // servletRegistrationBean.addInitParameter("deny", "192.168.0.114");
+        //控制台管理用户
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        //是否能够重置数据 禁用HTML页面上的“Reset All”功能
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+ 
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> filterRegistrationBean() {
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<WebStatFilter>(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+	
+}
