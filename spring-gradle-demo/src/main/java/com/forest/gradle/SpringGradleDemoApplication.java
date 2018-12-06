968ca4792552a954962bbb5ff202741e0628124a
forest <forest@192.168.226.1>
2018-12-06 10:34:04 +0800

    1

---
+++ spring-gradle-demo/src/main/java/com/forest/gradle/SpringGradleDemoApplication.java
@@ -0,0 +1,12 @@
+package com.forest.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleDemoApplication.class, args);
+	}
+}
